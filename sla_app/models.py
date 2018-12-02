3117f2febd2bd66947828f8cf0d5b8e0113bb8e6
Konrad Adamczyk <konradadamczykk@gmail.com>
2018-12-02 19:35:23 +0100

    sla_app: Update KPI model to satisfy report 3 requirements
    Signed-off-by: Konrad Adamczyk <konradadamczykk@gmail.com>

--- sla_app/models.py
+++ sla_app/models.py
@@ -18,10 +18,13 @@ class KeyPerformanceIndicator(models.Model):
     name = models.CharField(max_length=128)
     aggregation_type = models.ForeignKey(AggregationType, on_delete=models.CASCADE)
     operator = models.ForeignKey(Operator, on_delete=models.CASCADE)
+    customer = models.CharField(max_length=50) # TODO: Change to Customer model
     value = models.DecimalField(max_digits=20, decimal_places=10)
     country = models.CharField(max_length=30, null=True, blank=True)
     comment = models.TextField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    kpi_count = models.BigIntegerField(null=True, blank=True)
+    symbol = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.name
