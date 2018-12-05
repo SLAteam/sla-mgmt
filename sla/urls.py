809d46f2dd367f548e318c1350b1a8b2f1a2cd01
pitdeer <sarna0925@gmail.com>
2018-12-05 07:58:21 +0100

    zmiana frontend-poc do brancha
    This reverts commit 9f69d98fcbdd7e8108ef5e26dea20d9889c74ddf.

--- sla/urls.py
+++ sla/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import include, url
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sla_app/', include('sla_app.urls')), #to przekieruje do aplikacji sla_app do pliku urls 
-    #url(r'^sla_app/', include('sla_app.urls')), #to przekieruje do aplikacji sla_app do pliku urls 
+    path('sla_app/', include('sla_app.urls')),
 ]
--- sla_app/urls.py
+++ sla_app/urls.py
@@ -1,8 +1,6 @@
-from django.conf.urls import include, url
-#from django.urls import patterns
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-   # url(r'^$',  views.index, name='index')]
-    #url(r'^$',  views.all_kpi, name='all_kpi')]
-   url(r'^$',  views.get_all_kpi, name='get_all_kpi')]
+    url(r'^$', views.index, name='index')]
+
