b714c301ee3eb4581bbe59f21f0baac742e767a5
Konrad Adamczyk <konradadamczykk@gmail.com>
2018-12-12 20:52:26 +0100

    sla_app: urls: Register views in urls
    Signed-off-by: Konrad Adamczyk <konradadamczykk@gmail.com>

--- sla_app/urls.py
+++ sla_app/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from . import views
+from sla_app import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')]
+    url(r'^$', views.index, name='index'),
+    url('kpi/', views.kpi, name='kpi')
+]
 
