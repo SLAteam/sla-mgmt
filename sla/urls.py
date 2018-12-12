ec7d3eb441159e9b5ef684a8a97a459dbfc0aada
Konrad Adamczyk <konradadamczykk@gmail.com>
2018-12-12 20:50:14 +0100

    sla: urls: Set default URL for sla_app to blank
    Signed-off-by: Konrad Adamczyk <konradadamczykk@gmail.com>

--- sla/urls.py
+++ sla/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import sla_app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('sla_app.urls'))
 ]
