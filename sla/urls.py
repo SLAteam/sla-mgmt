26dd371ba3c2907eb8cd08966dafab21cbe57e7a
Piotr Sarna <sarna0925@gmail.com>
2018-12-19 03:16:53 +0100

    Dodanie panelu logowania

--- sla/urls.py
+++ sla/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 import sla_app
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('sla_app.urls'))
+    url(r'^$', include('sla_app.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^account/', include('accounts.urls'))
+  #  path('', include('sla_app.urls'))
 ]
