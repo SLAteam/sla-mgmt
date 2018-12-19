d835d1320cb460c01caa865455c5e337a8c20b7e
Piotr Sarna <sarna0925@gmail.com>
2018-12-19 03:16:53 +0100

    Dodanie panelu logowania

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import path
+
+urlpatterns = [
+    url(r'^$', views.home),
+    url(r'^loggedin$', views.loggedin),
+    #url(r'^login/$', auth_views.LoginView.as_view(), {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+]
