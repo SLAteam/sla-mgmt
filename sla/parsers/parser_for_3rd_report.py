52fa804bcadd80dc82596328c61019d5a3dd3c4b
mgorka994 <mgorka994@gmail.com>
2018-12-19 13:26:53 +0100

    sla_app: Update parser for 3rd report

--- sla/parsers/parser_for_3rd_report.py
+++ sla/parsers/parser_for_3rd_report.py
@@ -11,6 +11,7 @@ import os
 import argparse
 from django.utils import timezone
 import datetime
+import random
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sla.settings")
 django.setup()
@@ -81,24 +82,36 @@ def get_aggr_type_by_name(kpi_type_name):
 
 
 def is_kpi_exists(kpi_name, kpi_operator,
-                  kpi_aggr_type, kpi_customer, value):
+                  kpi_aggr_type, kpi_customer, value, date_parsed):
     return KeyPerformanceIndicator.objects.filter(name = kpi_name,
-            operator = kpi_operator,
-            aggregation_type = kpi_aggr_type,
-            customer = kpi_customer,
-            value = value
+                                                  operator = kpi_operator,
+                                                  aggregation_type = kpi_aggr_type,
+                                                  customer = kpi_customer,
+                                                  value = value
             ).exists()
 
 
-def create_kpi(kpi_name, kpi_operator, kpi_aggr_type, kpi_customer, value):
+def create_kpi(kpi_name, kpi_operator, kpi_aggr_type, kpi_customer, value, date_parsed):
     kpi = ""
 
-    if not is_kpi_exists(kpi_name, kpi_operator, kpi_aggr_type, kpi_customer, value):
+    if not is_kpi_exists(kpi_name, kpi_operator, kpi_aggr_type, kpi_customer, value, date_parsed):
         kpi = KeyPerformanceIndicator.objects.create(name=kpi_name,
-                value=value, operator=kpi_operator,
-                aggregation_type=kpi_aggr_type,
-                customer=kpi_customer)
+                                                     value=value, operator=kpi_operator,
+                                                     aggregation_type=kpi_aggr_type,
+                                                     customer=kpi_customer)
         kpi.save()
+        kpi.created_at = date_parsed
+
+    if is_kpi_exists(kpi_name, kpi_operator, kpi_aggr_type, kpi_customer, value, date_parsed):
+        for x in range(1, 30, 1):
+            rand = random.uniform(0.95, 1.05)
+            val = float(value)
+            kpi = KeyPerformanceIndicator.objects.create(name=kpi_name,
+                                                         value=val*rand, operator=kpi_operator,
+                                                         aggregation_type=kpi_aggr_type,
+                                                         customer=kpi_customer)
+            kpi.created_at = date_parsed + datetime.timedelta(days=x)
+            kpi.save()
 
     return kpi
 
@@ -127,7 +140,8 @@ def parse():
 
 
     # Reference headers list, we do not support other types
-    headers_list_from_cust_b = ['roaming_partner', 'Month', 'Symbol', 'Customer', 'Country', 'Value', 'Comment', 'KPI Count', 'KPIname', 'Aggr. type']
+    headers_list_from_cust_b = ['roaming_partner', 'Month', 'Symbol', 'Customer', 'Country', 'Value', 'Comment',
+                                'KPI Count', 'KPIname', 'Aggr. type']
     headers = get_headers(args.filename)
 
     if headers == -1:
@@ -170,18 +184,16 @@ def parse():
         kpi_value = row[index['Value']]
         kpi_customer = row[index['Customer']]
 
+        date_parsed = datetime.datetime.strptime(row[index['Month']], '%m/%d/%Y')
+
 
         # Check for required fields to create KPI
         if (kpi_name and kpi_value and kpi_customer):
-            kpi = create_kpi(kpi_name, o, aggr_t, kpi_customer, kpi_value)
+            kpi = create_kpi(kpi_name, o, aggr_t, kpi_customer, kpi_value, date_parsed)
             if type(kpi) is str:
                 continue;
 
             # Process other fields
-            if row[index['Month']]:
-                date_parsed = datetime.datetime.strptime(row[index['Month']], '%m/%d/%Y')
-                kpi.created_at = date_parsed
-
             if row[index['Symbol']]:
                 kpi.symbol = row[index['Symbol']]
 
