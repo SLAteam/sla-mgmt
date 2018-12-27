d622ba1df09f3e772e3c3a1e8bda3f337e69c5c9
Piotr Sarna <sarna0925@gmail.com>
2018-12-27 19:17:08 +0100

    połączenie auth i rebase

--- sla_app/urls.py
+++ sla_app/urls.py
@@ -6,6 +6,7 @@ from sla_app import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     path('viewer/', views.get_kpi_by_user, name='get_kpi_by_user'),
+    path('loggedin/', views.loggedin, name='loggedin'),
     path('viewer/<str:oper_id>', views.get_kpi_by_oper, name='get_kpi_by_oper')
 ]
 
