e3af9a1dea20e946ad7684eef738d657e2e7273a
Piotr Sarna <sarna0925@gmail.com>
2018-12-27 22:58:32 +0100

    Logowanie i przekierowanie na endpoint zależnie od statusu

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def home(request):
-    numbers = [1,2,3,4,5]
-    name = 'your name Sir/Madam ! :)'
-    
-    args = {'myName': name}
-    return render(request, 'accounts/home.html', args)
-
 def loggedin(request):
 	return HttpResponse('You have been logged in!')
+
+def index(request):
+	return render(request, 'accounts/index.html')
